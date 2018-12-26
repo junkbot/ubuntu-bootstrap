8f3ccb05bac1e19405ccca68742e6f57d49be5a0
junkbot <jerichowalls7@gmail.com>
2018-12-26 17:45:55 +1100

    Some mods

--- core/vim.sh
+++ core/vim.sh
@@ -1,6 +1,6 @@
 echo "[ Vim setup ]"
-echo "Copying dotfile to /usr/share/vim/vimrc ..."
-sudo cp $repo/conf/vim/vimrc /usr/share/vim/vimrc
+echo "Copying dotfile to $HOME/ ..."
+sudo cp $repo/conf/vim/vimrc $HOME/
 echo "Creating local directory $HOME/.vim/colors ..."
 mkdir -p $HOME/.vim/colors
 echo "Copying vim color schemes ..."
